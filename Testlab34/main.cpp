004486c56c16cc75ffba2f0b32fe3bdb0a26b03b
Pivinter <70973362+Pivinter@users.noreply.github.com>
2022-09-25 16:51:37 +0300

    Add project files.

---
+++ Testlab34/main.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+// Lab_03_4.cpp
+// < Кулявець Владислав Романович >
+// Лабораторна робота № 3.4
+// Розгалуження, задане плоскою фігурою.
+// Варіант 012
+#include <iostream>
+using namespace std;
+int main()
+{
+	double R; // вхідний параметр
+	double x; // вхідний аргумент
+	double y; // вхідний параметр
+	cout << "R = "; cin >> R;
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+
+	// розгалуження в повній формі
+	if ((y <= R + sqrt(R*R + (x-R)* (x - R)) && y >= 0 && x <= 0) ||
+		(y <= -R + sqrt(R * R + (x + R) * (x + R)) && x >= 0 && y <= 0))
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+	cin.get();
+	return 0;
+}
